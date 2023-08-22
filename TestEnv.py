5e8726affcdc3df7a3a0b18ecc3d354e2ed95eb5
wolf-syndrome <wolfhunter7521@gmail.com>
2023-08-22 14:24:57 +1000

    Created testfile to test if conda envr working

---
+++ TestEnv.py
@@ -0,0 +1,8 @@
+import torch
+
+
+print("Hello world")
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+if not torch.cuda.is_available():
+    print("Warning couldn't find cuda using cpu.")
