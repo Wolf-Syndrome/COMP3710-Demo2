ede9b1b2400caea2493232bd594a4207f0f21c07
wolf-syndrome <wolfhunter7521@gmail.com>
2023-09-12 04:45:55 +1000

    Fixed model by adding torch cat

--- Part3.py
+++ Part3.py
@@ -6,10 +6,10 @@ from time import perf_counter
 from os import listdir, path as ospath
 import natsort
 from PIL import Image
-import matplotlib.pyplot as plt
-
 
 trainning = True
+if not trainning:
+    import matplotlib.pyplot as plt
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 if not torch.cuda.is_available():
@@ -87,58 +87,65 @@ if not trainning:
 #https://lmb.informatik.uni-freiburg.de/people/ronneber/u-net/
 class UNet(nn.Module):
     def __init__(self, in_planes) -> None:
-        super(UNet, self).__init__()
+        super(UNet, self).__init__()        
 
-        self.test1 = nn.Conv2d(in_planes, 64, kernel_size=3, stride=1, padding=1)
-        self.test2 = nn.ReLU(inplace=True)
-        self.test3 = nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1)
-        self.test4 = nn.ReLU(inplace=True)
-        self.test5 = nn.MaxPool2d(kernel_size=2, stride=2)
-        
+        self.sec1 = self._make_layer(in_planes, 64, 64)
+        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.sec2 = self._make_layer(64, 128, 128)
+        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.sec3 = self._make_layer(128, 256, 256)
+        self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.sec4 = self._make_layer(256, 512, 512)
+        self.pool4 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.sec5 = self._make_layer(512, 1024, 1024)
 
-        self.sec1 = self._make_layer(in_planes, 64, 64, True)
-        self.sec2 = self._make_layer(64, 128, 128, True)
-        self.sec3 = self._make_layer(128, 256, 256, True)
-        self.sec4 = self._make_layer(256, 512, 512, True)
-        self.sec5 = self._make_layer(512, 1024, 1024, False)
-        self.sec6 = self._make_layer(1024, 512, 512, False)
-        self.sec7 = self._make_layer(512, 256, 256, False)
-        self.sec8 = self._make_layer(256, 128, 128, False)
-        self.sec9 = nn.Sequential(
-            nn.Conv2d(128, 64, kernel_size=3, stride=1, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1),
-            nn.ReLU(inplace=True),
-        )
+        self.upconv1 = nn.ConvTranspose2d(in_channels=1024, out_channels=1024//2,
+                                kernel_size=2, stride=2, padding=1)
+        self.sec6 = self._make_layer(1024, 512, 512)
+        self.upconv2 = nn.ConvTranspose2d(in_channels=512, out_channels=512//2,
+                                kernel_size=2, stride=2, padding=1)
+        self.sec7 = self._make_layer(512, 256, 256)
+        self.upconv3 = nn.ConvTranspose2d(in_channels=512, out_channels=512//2,
+                                kernel_size=2, stride=2, padding=1)
+        self.sec8 = self._make_layer(256, 128, 128)
+        self.upconv4 = nn.ConvTranspose2d(in_channels=512, out_channels=512//2,
+                                kernel_size=2, stride=2, padding=1)
+        self.sec9 = self._make_layer(128, 64, 64)
         self.sec10 = nn.Conv2d(64, 1, kernel_size=1, stride=1)
 
-    def _make_layer(self, in_planes, filter1, filter2, down):
-        if down:
-            lastLayer = nn.MaxPool2d(kernel_size=2, stride=2)
-        else:
-            lastLayer = nn.ConvTranspose2d(in_channels=in_planes, 
-                                           out_channels=in_planes//2, 
-                                           kernel_size=2, 
-                                           stride=2,
-                                           padding=1)
+    def _make_layer(self, in_planes, filter1, filter2):
         return nn.Sequential(
             nn.Conv2d(in_planes, filter1, kernel_size=3, stride=1, padding=1),
             nn.ReLU(inplace=True),
             nn.Conv2d(filter1, filter2, kernel_size=3, stride=1, padding=1),
             nn.ReLU(inplace=True),
-            lastLayer
         )
     
     def forward(self, out):
-        out = self.sec1(out)
-        out = self.sec2(out)
-        out = self.sec3(out)
-        out = self.sec4(out)
-        out = self.sec5(out)
+        # Encoder
+        xs1 = self.sec1(out)
+        xs2 = self.sec2(self.pool1(xs1))
+        xs3 = self.sec3(self.pool2(xs2))
+        xs4 = self.sec4(self.pool3(xs3))
+        xs5 = self.sec5(self.pool4(xs4))
+
+        # Decoder
+        xu5 = self.upconv1(xs5)
+        out = torch.cat([xu5, xs4], dim=1)
         out = self.sec6(out)
+
+        xu6 = self.upconv2(out)
+        out = torch.cat([xu6, xs3], dim=1)
         out = self.sec7(out)
+        
+        out = self.upconv3(out)
+        out = torch.cat([out, xs2], dim=1)
         out = self.sec8(out)
+        
+        out = self.upconv4(out)
+        out = torch.cat([out, xs1], dim=1)
         out = self.sec9(out)
+        
         out = self.sec10(out)
         return out
 
